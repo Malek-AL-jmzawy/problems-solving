f22b6622e2024d1ed9d3a35f70c1d2dfa79dc2ae
malek88888 <jmzomalek@gmail.com>
2020-12-12 17:51:15 +0200

    Q22 the arrays need to to be joind in one arrey

--- Q22/main.js
+++ Q22/main.js
@@ -10,17 +10,50 @@ console.log('Problem Solving Q: 22');
 
 function characterFrequency(input) {
   // YOUR CODE HERE
-  const strArr=input.split("")
-  const cleanArr =[]
-  for (let index = 0; index < strArr.length; index++) {
-    if (cleanArr.indexOf(strArr[index])==-1) {
-      cleanArr.push(strArr[index])
-    }    
+  const strArr = input.split("")
+  // const cleanArr = []
+  // for (let index = 0; index < strArr.length; index++) {
+  //   if (cleanArr.indexOf(strArr[index]) == -1) {
+  //     cleanArr.push(strArr[index])
+  //   }
+  // }
+  const arrSort = strArr.sort()
+  console.log("arrSort", arrSort);
+  // var count = [];
+  // strArr.forEach((i) =>{ count[i] = (count[i]||0) + 1;});
+  // console.log(count);
+  // console.log(cleanArr);
+  const countArr = []
+  var count = 1
+  for (let index = 1; index < arrSort.length; index++) {
+    if (arrSort[index] === arrSort[index - 1]) {
+      count++
+      // countArr.push(count)
+      console.log(count);
+    } 
+    if (!(arrSort[index] === arrSort[index - 1])) {
+      countArr.push(count)
+      count = 1
+      console.log("count2", count);
+    }
+    // else {
+    //   countArr.push(count)
+    //   count = 1
+    //   console.log("count2", count);
+    // }
   }
-  console.log(cleanArr);
+  // const m =arrSort[length - 1]
+  // console.log("arrSort[length - 1]" ,m );
+  if (arrSort[arrSort.length - 1] === arrSort[arrSort.length - 2]) {
+    countArr[countArr.length - 1]++
+  } else {
+    countArr.push(1)
+  }
+  console.log("countArr", countArr);
 }
 
-/* 
+
+/*
 Examples:
 characterFrequency('mississippi') // =>  [ ['i', 4],['s', 4],  ['p', 2],  ['m', 1]  ]
 characterFrequency('miaaiaaippi') // =>  [ ['a', 4],  ['i', 4],  ['p', 2],  ['m', 1] ]
