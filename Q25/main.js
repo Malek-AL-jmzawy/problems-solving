4465bd7f0bc0aeba120de5f80fbc68f224b2a17e
malek88888 <jmzomalek@gmail.com>
2020-12-13 15:47:07 +0200

    Q25 solved

---
+++ Q25/main.js
@@ -0,0 +1,20 @@
+console.log('Problem Solving Q: 25');
+
+/*is_array   */
+
+/*  
+Write a JavaScript function to check whether an `input` is an array or not.
+*/
+
+function is_array(input) {
+  // YOUR CODE HERE
+  const output=  Array.isArray(input)
+  return output
+}
+
+/* 
+Examples:
+is_array('w3resource') => false
+is_array ([1, 2, 4, 0]) // => true
+
+*/
