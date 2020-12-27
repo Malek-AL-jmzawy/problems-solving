3df643d1df8ac752a35ecb05faf4d302f6a447aa
malek88888 <jmzomalek@gmail.com>
2020-12-27 11:05:18 +0200

    Q9 solved

---
+++ 02_Data Structures + Algorithms [27 Q]/09_steps/index.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n) {                                         
+    let count=1
+    for (let index = 0; index < n; index++) {     
+        console.log("#".repeat(count++));   
+    }
+}
+
+module.exports = steps;
